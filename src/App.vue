f74d7e03d84365d76af701a8dac62a01eb96ce42
trohini31383 <53841712+trohini31383@users.noreply.github.com>
2019-11-18 21:14:25 -0800

    updated app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,125 +1,76 @@
 <template>
-
-  <div class='container'>
-
-    <h1> Your Todo List </h1>
-
-    <md-card class="todoBox">
-
-      <md-field>
-
-        <md-input v-model="currentTodo" 
-
-        @keydown.enter="addTodo()" 
-
-        placeholder="Add a todo">
-
-        </md-input>
-
-      </md-field>
-
-      <div class="todos">
-
-        <li v-for="(todo, index) in todos" :key="todo.id">
-
-          <input class ='completeButton' type='checkbox' v-model="todo.completed">
-
-              <span
-
-              class="todo-item-label"
-
-              :class='{completed: todo.completed}' 
-
-              @dblclick='editTodo(todo)' 
-
-              v-if="!todo.edit">
-
-                {{todo.label}}
-
-              </span>
-
-              <md-input v-else class="todo-item-edit" 
-
-              type="text" v-model='todo.label'
-
-              @keyup.enter="completedEdit(todo)"> 
-
-              </md-input>
-
-          <md-raised class='closeButton' @click="removeTodo(index)">Close</md-raised>
-
-        </li>
-
-      </div>
-
-    </md-card>
-
-  </div>
-
+  <md-app>
+    <md-app-toolbar class="md-primary">
+      <span class="md-title">Todo App</span>
+    </md-app-toolbar>
+    <md-app-content>
+      <md-list class="todos">
+        <md-list-item>
+          <md-field>
+            <md-input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo"></md-input>
+          </md-field>
+        </md-list-item>
+        <span v-for="todo in todos" :key="todo.id">
+          <todoItem
+            :todo="todo"
+            @toggle-completed="toggleTodo(todo)"
+            @remove="removeTodo(todo)"
+            @toggle-editing="toggleEditing(todo)"
+            @finish-editing="pushEditChanges"
+          ></todoItem>
+        </span>
+      </md-list>
+    </md-app-content>
+  </md-app>
 </template>
 
 
 
 <script>
-
+import todoItem from "./components/todo-item";
 export default {
-
-  data() {
-
-    return {
-
-      todos: [],
-
-      currentTodo: '',
-
-      
-
-    };
-
+  name: "app",
+  components: {
+    todoItem
   },
-
+  data() {
+    return {
+      todos: [],
+      currentTodo: ""
+    };
+  },
   methods: {
-
     addTodo() {
-
-      this.todos.push({
-
-        id: this.todos.length, 
-
-        label: this.currentTodo, 
-
-        completed: false,
-
-        edit: false
-
+      if (this.currentTodo) {
+        this.todos.push({
+          id: this.todos.length,
+          label: this.currentTodo,
+          completed: false,
+          isEditing: false
         });
-
-      this.currentTodo = ''
-
+        this.currentTodo = "";
+      }
     },
-
-    removeTodo(index) {
-
+    removeTodo(todo) {
+      const index = this.todos.indexOf(todo);
       this.todos.splice(index, 1);
-
     },
-
-    editTodo(todo) {
-
-     todo.edit = true;
-
+    toggleEditing(todo) {
+      const index = this.todos.indexOf(todo);
+      this.todos[index].isEditing = !this.todos[index].isEditing;
     },
-
-    completedEdit(todo) {
-
-      todo.edit = false;
-
+    pushEditChanges(payload) {
+      const [todo, newLabel] = payload;
+      const index = this.todos.indexOf(todo);
+      this.todos[index].label = newLabel;
+      this.toggleEditing(todo);
+    },
+    toggleTodo(todo) {
+      const index = this.todos.indexOf(todo);
+      this.todos[index].completed = !this.todos[index].completed;
     }
-
   }
-
 };
-
 </script>
 
 
---
+++ src/components/todo-item.vue
@@ -0,0 +1,37 @@
+<template>
+  <span>
+    <md-list-item v-if="!todo.isEditing">
+      <i
+        class="material-icons"
+        @click="$emit('toggle-completed')"
+      >{{ todo.completed ? "check_box" : "check_box_outline_blank" }}</i>
+      <span
+        :class="{ isComplete: todo.completed}"
+        @dblclick="$emit('toggle-editing')"
+      >{{ this.todo.label }}</span>
+      <i class="material-icons" @click="$emit('remove')">close</i>
+    </md-list-item>
+    <md-list-item v-if="todo.isEditing">
+      <md-field>
+        <md-input v-model="newLabel" @keydown.enter="$emit('finish-editing', [todo, newLabel])"></md-input>
+      </md-field>
+    </md-list-item>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: Object
+  },
+  data: function() {
+    return {
+      newLabel: this.todo.label
+    };
+  },
+  name: "todoItem"
+};
+</script>
+
+<style>
+</style>
